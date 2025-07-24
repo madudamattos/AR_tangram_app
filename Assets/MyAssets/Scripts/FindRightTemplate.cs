ab65ecc703cc594b9deeaec2614950c016bf5646
Maria Eduarda Mattos <130181995+madudamattos@users.noreply.github.com>
2025-07-24 13:05:24 -0300

    Todas as peças funcionando, bolinha cinza continua aparecendo

--- Assets/MyAssets/Scripts/FindRightTemplate.cs
+++ Assets/MyAssets/Scripts/FindRightTemplate.cs
@@ -13,20 +13,55 @@ public class FindRightTemplate : MonoBehaviour
         templatePiece = FindTemplate();
     }
 
+    /*    public GameObject FindTemplate()
+        {
+            pieceName = this.gameObject.name;
+
+            string pieceNumber = pieceName.Substring((pieceName.Length - 3), 3);
+
+            string templateName = "Template" + "." + pieceNumber;
+
+            templatePiece = GameObject.Find(templateName);
+
+            originalMat = templatePiece.transform.Find("Mesh").gameObject.GetComponent<Renderer>().material;
+
+            return templatePiece;
+        }*/
+
     public GameObject FindTemplate()
     {
         pieceName = this.gameObject.name;
-      
         string pieceNumber = pieceName.Substring((pieceName.Length - 3), 3);
-      
-        string templateName = "Template" + "." + pieceNumber;
+        string templateName = "Template." + pieceNumber;
 
-        templatePiece = GameObject.Find(templateName);
+        GameObject found = GameObject.Find(templateName);
 
-        originalMat = templatePiece.transform.Find("Mesh").gameObject.GetComponent<Renderer>().material;
+        if (found == null)
+        {
+            Debug.LogError($"[FindTemplate] Template '{templateName}' não encontrado.");
+            return null;
+        }
 
-        return templatePiece;
+        Transform mesh = found.transform.Find("Mesh");
+        if (mesh == null)
+        {
+            Debug.LogError($"[FindTemplate] O objeto '{templateName}' não contém um filho chamado 'Mesh'.");
+            return found;
+        }
+
+        Renderer r = mesh.GetComponent<Renderer>();
+        if (r == null)
+        {
+            Debug.LogError($"[FindTemplate] O objeto '{mesh.name}' não possui um componente Renderer.");
+        }
+        else
+        {
+            originalMat = r.material;
+        }
+
+        return found;
     }
+
 
     public GameObject GetTemplate()
     {
